a58abbdffeb2d0a40580cc21ae36024c43e5d289
dylan-deepshard <dylan@deepshard.org>
2024-12-16 12:32:45 -0800

    Create hisnameisyang.py

---
+++ hisnameisyang.py
@@ -0,0 +1,9 @@
+#A sample app for the Truffle^1 
+#Please provide feedback in this repo! We are actively developing this and want it to be what you want!
+
+from typing import List, Dict
+import os
+
+import truffle
+
+class HisNameIsYang:
